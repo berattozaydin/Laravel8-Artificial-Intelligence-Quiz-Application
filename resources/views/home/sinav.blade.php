3edb31005c011063787242965afea3db9c5a6973
Berat Özaydin <berattozaydin@gmail.com>
2022-04-24 01:00:12 +0300

    Quiz Time Added
    Quiz Time Added

--- resources/views/home/sinav.blade.php
+++ resources/views/home/sinav.blade.php
@@ -2,19 +2,44 @@
 @section('content')
     <div id="page-title" class="padding-tb-30px gradient-white">
         <div class="container text-left">
-            <h1 class="font-weight-300">{{$sinav->title}}</h1>
+        	<div class="w-75 mx-auto">
+            <div class="row">
+            <div class="col-12 col-md-3 text-center">
+            	<h1 class="font-weight-300">{{$sinav->title}}</h1>
+            </div> 
+            <div class="col-12 col-md-9 d-flex">	
+            	<div class="ml-md-auto mx-auto mx-md-0"><h2>Kalan Süreniz</h2> <h2 class="text-center" id="saniye"></h2></div>
+            </div>
+            </div>
+        </div>
         </div>
     </div>
+<script type="text/javascript">
+var deger;
+var saniye=3601;
+function saniyeBaslat()
+{
+	saniye --;
+	if(saniye >=0){
+		document.getElementById('saniye').innerHTML=((saniye/60).toFixed(2));
+	}else{
+		window.clearInterval(deger);
+		alert("Süreniz bitti! Sistemden Çıkış Yapılıyor.");
+		 window.location.href = "http://beratozaydin.org/logout";
+	}
+}
+var deger=window.setInterval('saniyeBaslat()',1000);
+</script>
+
 
 
     <div class="container">
-        <div class="row">
-
-            <div class="col-lg-8">
-                <div class="margin-bottom-40px card border-0 box-shadow">
+    	<div class="w-75 mx-auto">
+       	
+                <div class="margin-bottom-40px mx-auto card border-0 box-shadow">
                     <div class="padding-lr-30px padding-tb-20px">
                         <hr>
-                        <h3>Sınav Soruları</h3>
+                        <div><h3>Sınav Soruları</h3></div>
                         <div class="card-body">
                             <div class="card-text">
                                 <form method="POST" action="{{route('quiz.sonuc',$sinav->slug)}}">
@@ -61,7 +86,7 @@
                     </div>
                 </div>
 
-            </div>
-        </div>
+           </div>
+        
     </div>
 @endsection
